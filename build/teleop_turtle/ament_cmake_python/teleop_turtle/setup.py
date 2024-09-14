8f043628e23c039bbc978401d7fbd3649e4bc945
Kireiji02 <martmanaswin@gmail.com>
2024-09-15 00:26:44 +0700

    eat_error_can't solve

--- build/teleop_turtle/ament_cmake_python/teleop_turtle/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='teleop_turtle',
-    version='0.0.0',
-    packages=find_packages(
-        include=('teleop_turtle', 'teleop_turtle.*')),
-)
